e0e79319019b008934ec34d5f8d68fd6b3c0c439
Jae Eun Na <lette1394@gmail.com>
2019-04-14 18:49:15 +0900

    Add command line test for init command
    프로그램 시작하고 새로운 게임을 초기화

---
+++ jae-eun-na-1/src/test/java/com/jaeeunna/MainTest.java
@@ -0,0 +1,38 @@
+package com.jaeeunna;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayName("Program should handle")
+class MainTest {
+
+    @Test
+    @DisplayName("init command")
+    void init_command() {
+        // given
+        final String userInputLine = "init\n" +
+                "JaeEun\n" +
+                "Gil-dong\n" +
+                "Computer\n" +
+                "@DONE\n";
+        final OutputStream out = new ByteArrayOutputStream();
+        System.setIn(new ByteArrayInputStream(userInputLine.getBytes()));
+        System.setOut(new PrintStream(out));
+
+        // when
+        Main.main(null);
+
+        // then
+        final String expected = "----> Input player names, when you finished, input \"@DONE\"\n" +
+                "----> Now, Game Start.\n" +
+                "----> Frame 1.\n";
+        assertThat(out).isEqualTo(expected);
+    }
+}
