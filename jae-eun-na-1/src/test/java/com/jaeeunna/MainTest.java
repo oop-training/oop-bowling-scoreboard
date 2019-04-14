be38008f7172a96a51e1def61a0426aedc19f09d
Jae Eun Na <lette1394@gmail.com>
2019-04-14 19:47:58 +0900

    Add test for init command when there is already stared game

--- jae-eun-na-1/src/test/java/com/jaeeunna/MainTest.java
+++ jae-eun-na-1/src/test/java/com/jaeeunna/MainTest.java
@@ -1,6 +1,7 @@
 package com.jaeeunna;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayInputStream;
@@ -12,27 +13,54 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("Program should handle")
 class MainTest {
-
-    @Test
+    @Nested
     @DisplayName("init command")
-    void init_command() {
-        // given
-        final String userInputLine = "init\n" +
-                "JaeEun\n" +
-                "Gil-dong\n" +
-                "Computer\n" +
-                "@DONE\n";
-        final OutputStream out = new ByteArrayOutputStream();
-        System.setIn(new ByteArrayInputStream(userInputLine.getBytes()));
-        System.setOut(new PrintStream(out));
+    class InitCommand {
+        @Test
+        @DisplayName("when clean start")
+        void init_command_clean_start() {
+            // given
+            final String userInputLine = "init\n" +
+                    "JaeEun\n" +
+                    "Gil-dong\n" +
+                    "Computer\n" +
+                    "@DONE\n";
+            final OutputStream out = new ByteArrayOutputStream();
+            System.setIn(new ByteArrayInputStream(userInputLine.getBytes()));
+            System.setOut(new PrintStream(out));
 
-        // when
-        Main.main(null);
+            // when
+            Main.main(null);
 
-        // then
-        final String expected = "----> Input player names, when you finished, input \"@DONE\"\n" +
-                "----> Now, Game Start.\n" +
-                "----> Frame 1.\n";
-        assertThat(out).asString().isEqualTo(expected);
+            // then
+            final String expected = "----> Input player names, when you finished, input \"@DONE\"\n" +
+                    "----> Now, Game Start.\n" +
+                    "----> Frame 1.\n";
+            assertThat(out).asString().isEqualTo(expected);
+        }
+
+        @Test
+        @DisplayName("when there is already started game")
+        void init_command_there_is_already_started_game() {
+            final String userInputLine = "init\n" +
+                    "Yes\n" +
+                    "JaeEun\n" +
+                    "Gil-dong\n" +
+                    "Computer\n" +
+                    "@DONE\n";
+            final OutputStream out = new ByteArrayOutputStream();
+            System.setIn(new ByteArrayInputStream(userInputLine.getBytes()));
+            System.setOut(new PrintStream(out));
+
+            // when
+            Main.main(null);
+
+            // then
+            final String expected = "----> Are you sure? All changes will be discarded. (Y/N)\n" +
+                    "----> Input player names, when you finished, input \"@DONE\"\n" +
+                    "----> Now, Game Start.\n" +
+                    "----> Frame 1.\n";
+            assertThat(out).asString().isEqualTo(expected);
+        }
     }
 }
